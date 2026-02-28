5c0b0060539b681221b457c08ac3cbdf63fcc972
KalpnathRai <165119993+KalpnathRai@users.noreply.github.com>
2026-02-28 13:16:47 +0530

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "hub_vpc_ids" {
-  description = "Hub VPC IDs"
-  value       = module.hub_vpc.vpc_ids
-}
-
-output "hub_subnet_ids" {
-  description = "Hub Subnet IDs"
-  value       = module.hub_subnets.subnet_ids
-}
